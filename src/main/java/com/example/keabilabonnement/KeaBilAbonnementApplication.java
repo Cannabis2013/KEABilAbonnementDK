903b82112b8453b8746cfb73c355d405c1531ce0
Cannabis2013 <havnetrold2002@yahoo.dk>
2022-11-28 11:00:26 +0100

    Initial commit

---
+++ src/main/java/com/example/keabilabonnement/KeaBilAbonnementApplication.java
@@ -0,0 +1,13 @@
+package com.example.keabilabonnement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KeaBilAbonnementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KeaBilAbonnementApplication.class, args);
+    }
+
+}
