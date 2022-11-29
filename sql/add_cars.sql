92444ee4d63e203fde2718df69105790429d36e3
Cannabis2013 <MH2012@outlook.dk>
2022-11-29 13:48:00 +0100

    Added populate script

--- sql/add_cars.sql
+++
@@ -1,4 +0,0 @@
-insert into Car (Brand, Model, VIN, Number)
-values ("Opel","Kadet","ABCJW12WFSD2442WW","TS440"),
-("Nissan","Shitbox 1983","GFDAW6HGF3842FFA1","TS316"),
-("Fiat","Duna 70 1989","GPARKENFCK1992S12","TS116");
--- sql/add_customers.sql
+++
@@ -1,2 +0,0 @@
-insert into Customer (License_Id, Name, Birthday, Address, PhoneNumber)
-values ("12345678","Elon Musk","1971-06-23","Twitter Street 4, Los Angeles, USA","12121212");
